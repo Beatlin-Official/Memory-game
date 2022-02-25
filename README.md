f9f9e2ea8c8d87ba4d615c0ead2fc2631455321a
beatlin <art851025@gmail.com>
2022-02-25 16:08:42 +0800

    Add scoreboard function and update README.md

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
 <p align="center">
   <a href="https://beatlin-official.github.io/test/"  target="_blank" rel="noopener noreferrer">
-    <img width="400" src="./img/logo.png" alt="Vue logo">
+    <img width="400" src="./img/logo-whitebg.png" alt="Vue logo">
   </a>
 </p>
 
 
 ## Memory Game
-[game here](https://beatlin-official.github.io/test/).
+[遊戲遊玩 / Game Here](https://beatlin-official.github.io/test/).
+
+## 功能介紹
+
+### 運用MVC來模組化程式
+
+雖不影響使用者體驗，不過運用模組化在將來維護上會更加直觀與效率，避免因為局部修改而造成滾動式的修改。
+
+
+### RWD頁面
+
+對桌面以及手機裝置介面的體驗皆能無痛使用。
--- main.js
+++ main.js
@@ -23,6 +23,17 @@ const utility = {
         return number
     }
 }
+
+const model = {
+    revealedCards: [],
+    isRevealedCardsMatched() {
+        return this.revealedCards[0].dataset.index % 13 ===
+            this.revealedCards[1].dataset.index % 13
+    },
+    score: 0,
+    triedTimes: 0
+}
+
 const controller = {
     currectState: GAME_STATE.FirstCardAwaits,
     generateCards() {
@@ -34,43 +45,50 @@ const controller = {
         }
         switch (this.currectState) {
             case GAME_STATE.FirstCardAwaits:
-                view.flipCard(card)
+                view.flipCards(card)
                 model.revealedCards.push(card)
                 this.currectState = GAME_STATE.SecondCardAwaits
                 break
+
             case GAME_STATE.SecondCardAwaits:
-                view.flipCard(card)
+                view.renderTriedTimes(++model.triedTimes)
+                view.flipCards(card)
                 model.revealedCards.push(card)
                     //判斷是否成功
                 if (model.isRevealedCardsMatched()) {
+                    view.renderScore(model.score += 10)
                     this.currectState = GAME_STATE.CardsMatched
-                    view.pairCard(model.revealedCards[0])
-                    view.pairCard(model.revealedCards[1])
+                    view.pairCards(...model.revealedCards)
                     model.revealedCards = []
+
+                    if (model.score === 260) {
+                        this.currectState = GAME_STATE.GameFinished
+                        view.showGameFinished()
+                        return
+                    }
                     this.currectState = GAME_STATE.FirstCardAwaits
+
                 } else {
                     this.currectState = GAME_STATE.CardsMatchFailed
-                    setTimeout(() => {
-                        view.flipCard(model.revealedCards[0])
-                        view.flipCard(model.revealedCards[1])
-                        model.revealedCards = []
-                        this.currectState = GAME_STATE.FirstCardAwaits
-                    }, 1000)
+                    view.appendWrongAnimation(...model.revealedCards)
+                    setTimeout(this.resetCards, 900)
                 }
                 break
         }
         console.log('currectState', this.currectState)
         console.log('revealedCards', model.revealedCards.map(card =>
             card.dataset.index))
+    },
+    resetCards() {
+        view.flipCards(...model.revealedCards)
+        model.revealedCards = []
+        controller.currectState = GAME_STATE.FirstCardAwaits //resetCards會被當參數setTimeout給使用,this就必改為controller
+    },
+    restartGame() {
+
     }
 }
-const model = {
-    revealedCards: [],
-    isRevealedCardsMatched() {
-        return this.revealedCards[0].dataset.index % 13 ===
-            this.revealedCards[1].dataset.index % 13
-    }
-}
+
 const view = {
     getCardElement(index) {
         return `
@@ -86,18 +104,21 @@ const view = {
                 <p>${number}</p>
         `
     },
-    flipCard(card) {
-        if (card.classList.contains('back')) {
-            card.classList.remove('back')
-            card.innerHTML = this.getCardContent(Number(card.dataset.index))
-            return
-        }
-
-        card.classList.add('back')
-        card.innerHTML = null
+    flipCards(...cards) {
+        cards.map(card => {
+            if (card.classList.contains('back')) {
+                card.classList.remove('back')
+                card.innerHTML = this.getCardContent(Number(card.dataset.index))
+                return
+            }
+            card.classList.add('back')
+            card.innerHTML = null
+        })
     },
-    pairCard(card) {
-        card.classList.add('paired')
+    pairCards(...cards) {
+        cards.map(card => {
+            card.classList.add('paired')
+        })
     },
     transformNumber(number) {
         switch (number) {
@@ -117,9 +138,43 @@ const view = {
         const cards = document.querySelector('#cards')
         cards.innerHTML = indexes.map(index =>
             this.getCardElement(index)).join('')
+    },
+    renderScore(score) {
+        document.querySelector('.score').innerHTML = `Score: ${score}`
+    },
+    renderTriedTimes(times) {
+        document.querySelector('.tried').innerHTML = `You've tried: ${times} times`
+    },
+    appendWrongAnimation(...cards) {
+        cards.map(card => {
+            card.classList.add('wrong')
+            card.addEventListener('animationend', event =>
+                event.target.classList.remove('wrong'), { once: true })
+        })
+    },
+    showGameFinished() {
+        const div = document.createElement('div')
+        div.classList.add('completed')
+        div.innerHTML = `
+        <h1>Complete!</h1>
+        <p>Score: ${model.score}</p>
+        <p>You've tried: ${model.triedTimes} times</p>
+        <button class="restart">&#8635 Play again</button>
+        `
+        const header = document.querySelector('#header-container')
+        header.before(div)
+
+        return this.restartGame()
+    },
+    restartGame() {
+        const restart = document.querySelector('.restart')
+        restart.addEventListener('click', event => {
+            window.location.reload()
+        })
     }
 }
 controller.generateCards()
+    // 直接看結束畫面view.showGameFinished()
 
 document.querySelectorAll('.card').forEach(card => {
     card.addEventListener('click', event => {
